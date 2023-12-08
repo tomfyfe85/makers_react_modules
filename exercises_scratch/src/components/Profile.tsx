c25832fe6b9e8dd7e6a756414e45db2869fc8c25
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-08 21:51:29 +0000

    02_components exercises compete in makers_scratch

---
+++ exercises_scratch/src/components/Profile.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+const Profile = () => {
+  
+  
+  return (
+    <div id="profile">
+      <h1 id="name">Quakie Makers</h1>
+      <p id="job">Makers' fav duck</p>
+      <p id="birth_date">2023</p>
+    </div>
+  )
+}
+
+export default Profile;
