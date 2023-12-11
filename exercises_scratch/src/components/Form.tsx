6cc81981184649f7fb982f36f7f070e2a9f3bcc1
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-11 01:25:42 +0000

    08 complete

--- exercises_scratch/src/components/Form.tsx
+++ exercises_scratch/src/components/Form.tsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
 const Form = () => {
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("")
-  
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [count, setCount] = useState(0);
+
   const handleUsernameChange = (event) => {
     const inputEl = event.target;
-    setUsername(inputEl.value)
+    setUsername(inputEl.value);
   };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    const length = event.target.value.length;
+    setCount(length);
+    setPassword(event.target.value);
+    return length;
+  };
+
+  const handleNumberChange = (event) => {
+    const inputEl = event.target;
+
+    setPhoneNumber(inputEl.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     fetch("http://url.com/endpoint", {
       method: "POST",
-      body: JSON.stringify({ username: username, password: password })
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        phoneNumber: phoneNumber,
+      }),
     });
   };
 
   return (
-    <form onSubmit ={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <label>
         Enter your username:
         <input
@@ -39,10 +54,24 @@ const Form = () => {
           value={password}
           onChange={handlePasswordChange}
         />
+        <p>
+          {count <= 8
+            ? "password must be longer than 8 characters"
+            : "password ok"}
+        </p>
+      </label>
+      <label>
+        Enter your phone number:
+        <input
+          type="number"
+          name="number"
+          value={phoneNumber}
+          onChange={handleNumberChange}
+        />
       </label>
       <label>
         Submit
-        <input type="submit" name="submit"/>
+        <input type="submit" name="submit" />
       </label>
     </form>
   );
